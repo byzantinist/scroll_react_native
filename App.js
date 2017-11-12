a332b4b321bc08ad2b374e203ba44dbad19b3f98
byzantinist <echiang777@yahoo.com>
2017-11-12 16:31:11 -0600

    Replicate data from the work Eric and Charlie did, as well as Matt's portion

--- App.js
+++ App.js
@@ -1,30 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { StyleSheet, View } from 'react-native';
 
+import HomeScreen from './components/homescreen';
+import ScrollScreen from './components/scrollscreen';
+import ScrollContents from './components/scrollcontents';
 import MattTest from './components/matttest';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
+export default class App extends React.Component {
   render() {
     return (
-      <MattTest/>
+        <ScrollApp/>
     );
   }
 }
@@ -32,18 +18,13 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: 'pink',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    justifyContent: 'center'
+  }
+});
+
+export const ScrollApp = StackNavigator({
+  Home: { screen: HomeScreen },
+  Scroll: { screen: ScrollScreen }
 });
